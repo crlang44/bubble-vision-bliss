e1d2f08aeae441b07035bb69a2ea07f640b20b46
crlang44 <crlang44@gmail.com>
2025-06-02 15:31:17 -0700

    Remove music

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import OceanAnnotationGamePage from './pages/OceanAnnotationGamePage';
 import QuickIDGamePage from './pages/QuickIDGamePage';
 import NotFound from './pages/NotFound';
 import GroundTruthEditor from './pages/GroundTruthEditor';
-import { MusicProvider } from './components/MusicProvider';
-import MusicControl from './components/MusicControl';
-import MusicManager from './components/MusicManager';
+
 import './index.css';
 import './styles/ocean-styles.css';
 import { useClickSound } from './hooks/useClickSound';
@@ -17,20 +15,16 @@ function App() {
 
   return (
     <>
-      <MusicProvider>
-        <Router>
-          <MusicManager />
-          <Routes>
-            <Route path="/" element={<Navigate to="/ocean-annotation" replace />} />
-            <Route path="/ocean-annotation" element={<OceanAnnotationGamePage />} />
-            <Route path="/quick-id" element={<QuickIDGamePage />} />
-            <Route path="/ground-truth-editor" element={<GroundTruthEditor />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-        <MusicControl />
-        <Toaster />
-      </MusicProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/ocean-annotation" replace />} />
+          <Route path="/ocean-annotation" element={<OceanAnnotationGamePage />} />
+          <Route path="/quick-id" element={<QuickIDGamePage />} />
+          <Route path="/ground-truth-editor" element={<GroundTruthEditor />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Toaster />
     </>
   );
 }
